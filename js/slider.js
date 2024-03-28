6bc4e915d7e1e0140244d06172ede98798d292f3
SvitLime <spzhuk@gmail.com>
2024-03-28 15:14:28 +0200

    new file

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,49 @@
+// const firstSwiper = new Swiper('.first-slider', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     },
+//   // autoplay: {
+//   //       delay: 2500,
+//   //       disableOnInteraction: false,
+//   //     },
+
+
+// });
+
+// const secondSwiper = new Swiper('.second-slider', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//     },
+// });
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'horizontal',
+  direction: 'vertical',
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    },
-//   autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//       },
-
-
+  },
+  autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
 });
+
+// const secondSwiper = new Swiper('.second-slider', {
+//     direction: 'horizontal',
+//     loop: true,
+//     pagination: {
+//         el: '.pagination-second',
+//     },
+// });
